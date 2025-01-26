ccae08941b971190d8edc5c213cb3c58320a8fee
amantesf <tesfayee.amanuel@gmail.com>
2025-01-26 14:54:48 -0500

    Updated exchange rates, flags, country selection, etc

---
+++ script.js
@@ -0,0 +1,58 @@
+const dropList = document.querySelectorAll(".drop-list select");
+fromCurrency = document.querySelector(".from select");
+toCurrency = document.querySelector(".to select");
+getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+    for (let currency_code in country_list) { 
+        let selected;
+        if (i == 0) {
+            selected = currency_code == "USD" ? "selected" : "";
+        } else if (i == 1) {
+            selected = currency_code == "ETB" ? "selected" : "";
+        }
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+    dropList[i].addEventListener("change", e => {
+        loadFlag(e.target);
+    } );
+
+}
+
+function loadFlag(element){
+    for(code in country_list){
+        if(code == element.value){
+            let imgTag = element.parentElement.querySelector("img");
+            imgTag.src = `https://flagsapi.com/${country_list[code]}/flat/64.png`
+        }
+    }
+}
+
+window.addEventListener("load", () => {
+    getExchangeRate();
+});
+getButton.addEventListener("click", e=> {
+    e.preventDefault();
+    getExchangeRate();
+});
+
+
+function getExchangeRate(){
+    const amount = document.querySelector(".amount input");
+    exchangeRateTxt = document.querySelector(".exchange-rate");
+
+    let amountVal = amount.value;
+    if(amountVal == "" || amountVal == "0"){
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/4126e4607a632c849f5dfa65/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        console.log(exchangeRate);
+        let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    })
+}
